85e9e0ae74b5c149db75d7dd65445c1f5de712df
victooorltt <victorlasheras2006@gmail.com>
2026-06-21 14:01:32 +0200

    Logos fixed

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
           { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
-          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://www.googletagmanager.com; frame-src 'self' https://www.googletagmanager.com https://maps.google.com https://www.google.com; connect-src 'self';" }
+          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://www.googletagmanager.com https://cdn.simpleicons.org; frame-src 'self' https://www.googletagmanager.com https://maps.google.com https://www.google.com; connect-src 'self';" }
         ],
       },
     ];
